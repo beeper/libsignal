1567ea9b9f42c24535664aaa53836b3338bd1759
Tulir Asokan <tulir@maunium.net>
2026-07-30 12:27:58 +0300

    Revert "Fix Windows and 32-bit Android builds"
    This reverts commit c061f3f1812e9b3c3e70c0871f10b73bfd92df38.
    
    https://github.com/signalapp/libsignal/commit/2eba886f662f38aa04d8455f7503828cf1b4fb08 fixed it upstream

--- rust/bridge/shared/types/src/ffi/convert.rs
+++ rust/bridge/shared/types/src/ffi/convert.rs
@@ -1103,11 +1103,6 @@ pub struct FfiBulkPolledStreamTerminationReason {
     raw: *mut SignalFfiError,
 }
 
-#[cfg(not(windows))]
-const MAP_FAILED: *mut std::ffi::c_void = libc::MAP_FAILED;
-#[cfg(windows)]
-const MAP_FAILED: *mut std::ffi::c_void = std::ptr::without_provenance_mut(usize::MAX);
-
 impl<T: IntoFfiError> ResultTypeInfo for Option<BulkPolledStreamTerminationReason<T>> {
     type ResultType = FfiBulkPolledStreamTerminationReason;
 
@@ -1116,7 +1111,7 @@ impl<T: IntoFfiError> ResultTypeInfo for Option<BulkPolledStreamTerminationReaso
             Some(BulkPolledStreamTerminationReason::Error(e)) => {
                 crate::support::BridgedError(e).convert_into()?
             }
-            Some(BulkPolledStreamTerminationReason::Finished) => MAP_FAILED.cast(),
+            Some(BulkPolledStreamTerminationReason::Finished) => libc::MAP_FAILED.cast(),
             None => std::ptr::null_mut(),
         };
         Ok(FfiBulkPolledStreamTerminationReason { raw })
